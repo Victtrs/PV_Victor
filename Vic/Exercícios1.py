e3b4ab6bf0408541bf8bea31d466715ae78511c3
Victtrs <148557783+Victtrs@users.noreply.github.com>
2023-10-20 11:03:32 -0300

    Add files via upload

---
+++ Vic/Exercícios1.py
@@ -0,0 +1,13 @@
+#Exercício 1
+print("O primeiro programa a gente nunca esquece!")
+
+#Exercício 2
+print("Nome: Victor")
+print("Cidade: Bauru")
+print("e-mail: victorhugos.waf@gmail.com ")
+
+#exercício 3
+print("XXXXX")
+print("X   X")
+print("X   X")
+print("XXXXX")
